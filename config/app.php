b3d98191f014b6b75449ee009875fb2a834f9213
Vladimir Kochergin <wawan1331@gmail.com>
2016-08-12 10:36:17 +0300

    Install captcha

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
     ],
 
     /*
