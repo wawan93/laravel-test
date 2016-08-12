dfc9e3d353c4b0e09a8ffeeb6001bdba54b13b7a
Vladimir Kochergin <wawan1331@gmail.com>
2016-08-12 10:42:47 +0300

    Install IDE helper and Debug bar

--- config/app.php
+++ config/app.php
@@ -164,6 +164,9 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
+
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -209,6 +212,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
