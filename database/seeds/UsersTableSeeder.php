d24a071ceb3102c06c95010381640c44123c00e9
Vladimir Kochergin <vladimir.kochergin@rambler-co.ru>
2017-04-28 12:09:00 +0300

    Add default user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin'),
+            'role' => User::ROLE_MODERATOR
+        ]);
+    }
+}
